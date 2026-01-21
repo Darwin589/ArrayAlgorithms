aedb0f0bcc018dbc5efb5244406ffc908f3b291d
Darwin589 <chucochicho2009@gmail.com>
2026-01-21 12:24:27 -0500

    n

---
+++ ExampleOne.java
@@ -0,0 +1,13 @@
+//Darwin Castillo
+// 01/21/26
+//repostatory for all 12 examples
+public class ExampleOne{
+    public static void main(String[]args){
+    int [ ] nums = {-2,0,5,7,9};
+    for(int i=0;i<nums.length;i++){
+    }
+    for(int num: nums){
+        
+    }
+}
+}
